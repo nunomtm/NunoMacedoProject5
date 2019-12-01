6aa4bba3a146b86e2fd8aff2069b6d0c6ddf1f72
Nuno Macedo <nunomacedo@Nunos-MacBook-Air.local>
2019-12-01 08:58:04 -0500

    Updates

---
+++ precache-manifest.4b496307d3d7b5cdbcd037b7da41a41c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a922103c32e07e0bfb8dcfdbcee41de3",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/index.html"
+  },
+  {
+    "revision": "1ce2be56cf55a0474d38",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/css/main.82008a85.chunk.css"
+  },
+  {
+    "revision": "bca0848a5c76256c9e6f",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/js/2.635d72c6.chunk.js"
+  },
+  {
+    "revision": "1ce2be56cf55a0474d38",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/js/main.0fab0eae.chunk.js"
+  },
+  {
+    "revision": "b294d53609273ff55c7a",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/js/runtime-main.b3d177af.js"
+  },
+  {
+    "revision": "aa82d9488882ea36005dffb581b9afb4",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/BethEllenRegular.aa82d948.ttf"
+  },
+  {
+    "revision": "51e7b86c1328d728957c931df6f86dcc",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/apple.51e7b86c.png"
+  },
+  {
+    "revision": "73dcd6a11ff6566ab890f83943adc2b2",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/banana.73dcd6a1.png"
+  },
+  {
+    "revision": "94a6bfa978565eb2394fe55b52d6414f",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/cherry.94a6bfa9.png"
+  },
+  {
+    "revision": "938c9ea02060b5b65a8fc0c7284c583f",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/grapes.938c9ea0.png"
+  },
+  {
+    "revision": "2622bfd39471e756e187d36c5a034e8f",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/headerBackground.2622bfd3.jpg"
+  },
+  {
+    "revision": "c6610b9ea02ae0c81a66e1547b68d840",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/lettus.c6610b9e.png"
+  },
+  {
+    "revision": "a42e91866e795bf14b180d2ffce2f767",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/notePad.a42e9186.png"
+  },
+  {
+    "revision": "fc156e048def50874e09e40911e31de9",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/onion.fc156e04.png"
+  },
+  {
+    "revision": "514576673ae7d660e268433805f4d346",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/orange.51457667.png"
+  },
+  {
+    "revision": "a6d0f39f713a22dd302ba512bcc5336e",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/pineapple.a6d0f39f.png"
+  },
+  {
+    "revision": "bb8a8013b4f1bc918543c303e5dcfeae",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/resizedShelf.bb8a8013.png"
+  },
+  {
+    "revision": "3b5ce510f51a80ba6a629d0f62b5d868",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/strawberry.3b5ce510.png"
+  },
+  {
+    "revision": "821bf9d2022cab716aaf59b719840eac",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/tomato.821bf9d2.png"
+  },
+  {
+    "revision": "4197b5274118121547f2bb1aa1e8e144",
+    "url": "%20//github.com/nunomtm/NunoMacedoProject5/static/media/watermelon.4197b527.png"
+  }
+]);
